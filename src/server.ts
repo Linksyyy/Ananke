a025aab8f79745d4f16787ef684dfe877fea4432
Linksyyy <enzovidero@gmail.com>
2026-02-01 17:01:06 -0300

    Enhance log feedback

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import { createServer } from "http";
 import next from "next";
 
-const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handle = app.getRequestHandler();
 
 app.prepare().then(async () => {
-  const server = createServer((req, res) => handle(req, res));
+  const server = createServer((req, res) => {
+    const start = Date.now();
+
+    res.on("finish", () => {
+      const duration = Date.now() - start;
+      const { method, url } = req;
+      const status = res.statusCode;
+      const color = status >= 400 ? "\x1b[31m" : "\x1b[32m";
+
+      console.log(
+        `[${method} | ${color}${status}\x1b[0m] ${duration}ms | ${url}`,
+      );
+    });
+
+    handle(req, res);
+  });
 
   server.listen(3000, () => {
     console.log("Server is now active");
